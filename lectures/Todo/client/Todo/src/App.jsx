f70cefb5ae5db7c1b7fad9ebc427837828986538
Shawn Tapps <stapps.prsvr@gmail.com>
2025-01-23 19:12:18 -0600

    todo list

--- lectures/Todo/client/Todo/src/App.jsx
+++ lectures/Todo/client/Todo/src/App.jsx
@@ -7,11 +7,14 @@ function App() {
   // const [count, setCount] = useState(0)
 
   const [data, setData] = useState();
+  const [newTodo, setNewTodo] = useState("");
+  const [editingId, setEditingId] = useState(null);
+  const [editText, setEditText] = useState("");
 
   useEffect(() => {
     axios({
       method: "get",
-      url: "http://localhost:3000/gettodos",
+      url: "http://localhost:3000/getTodos",
     })
       .then((res) => {
         console.log("res", res);
@@ -20,21 +23,105 @@ function App() {
       .catch((err) => console.log("err", err));
   }, []);
 
-  return (
-    <>
-      {console.log("data", data)}
+  const handleSubmit = (e) => {
+    if (!newTodo.trim()) return;
+    //const todoItem = { todo: newTodo, created: new Date() };
+    e.preventDefault();
+    axios({
+      method: "post",
+      url: "http://localhost:3000/create",
+      data: {
+        todo: newTodo,
+        created: new Date(),
+      },
+    })
+      .then((res) => {
+        console.log("adding todo", res);
+        setData([...data, res.data]);
+        setNewTodo("");
+      })
+      .catch((err) => console.log("Error adding todo", err));
+  };
 
-      {data &&
-        data.map((item) => {
-          return (
-            <div style={{ border: "2px solid red" }}>
-              <p> {item.todo}</p>
-              <button>delete</button>
-              <button>edit</button>
-            </div>
-          );
-        })}
-    </>
+  const handleDelete = (id) => {
+    console.log("Deleting todo:", id);
+    axios({
+      method: "delete",
+      url: `http://localhost:3000/delete/${id}`,
+    })
+      .then((res) => {
+        console.log("Todo deleted:", res.data);
+        setData(data.filter((item) => item._id !== id));
+      })
+      .catch((err) => console.log("Error deleting todo:", err));
+  };
+
+  const handleEdit = (id) => {
+    const todoToEdit = data.find((item) => item._id === id);
+    setEditingId(id);
+    setEditText(todoToEdit.todo);
+  };
+
+  const handleUpdate = (id) => {
+    console.log("Updating todo:", id);
+    axios({
+      method: "put",
+      url: `http://localhost:3000/update/${id}`,
+      data: {
+        todo: editText,
+        created: new Date(),
+      },
+    })
+      .then((res) => {
+        console.log("Todo updated:", res.data);
+        setData(data.map((item) => (item._id === id ? res.data : item)));
+        setEditingId(null);
+        setEditText("");
+      })
+      .catch((err) => console.log("Error updating todo:", err));
+  };
+
+  return (
+    <div className="todo-container">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+
+      <div className="todo-list">
+        {data &&
+          data.map((item) => {
+            return (
+              <div key={item._id} className="todo-item">
+                {editingId === item._id ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editText}
+                      onChange={(e) => setEditText(e.target.value)}
+                    />
+                    <button onClick={() => handleUpdate(item._id)}>Save</button>
+                    <button onClick={() => setEditingId(null)}>Cancel</button>
+                  </>
+                ) : (
+                  <>
+                    <p>{item.todo}</p>
+                    <button onClick={() => handleDelete(item._id)}>
+                      Delete
+                    </button>
+                    <button onClick={() => handleEdit(item._id)}>Edit</button>
+                  </>
+                )}
+              </div>
+            );
+          })}
+      </div>
+    </div>
   );
 }
 
--- lectures/Todo/server/server.js
+++ lectures/Todo/server/server.js
@@ -1,56 +1,72 @@
-const express = require("express")
-const cors = require('cors')
-const app = express()
-const mongoose = require("mongoose")
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const mongoose = require("mongoose");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-require('dotenv').config()
+const PORT = 3000;
 
-const PORT = 3000
+const Schema = mongoose.Schema;
 
-const Schema = mongoose.Schema
-
-const ToDoSchema = new Schema(
-    {
-        todo: String,
-        created: Date
-    }
-)
-const ToDo = mongoose.model('ToDo', ToDoSchema)
+const ToDoSchema = new Schema({
+  todo: String,
+  created: Date,
+});
+const ToDo = mongoose.model("ToDo", ToDoSchema);
 
 app.get("/test", (req, res) => {
-    console.log("Test route hit")
-    res.json({ msg: "success" })
-})
+  console.log("Test route hit");
+  res.json({ msg: "success" });
+});
 
 app.get("/getTodos", (req, res) => {
-    console.log("getTodos HIT")
-    ToDo.find()
-        .then(found => {
-            console.log("Found", found)
-            res.json(found)
-        })
-})
+  console.log("getTodos HIT");
+  ToDo.find().then((found) => {
+    console.log("Found", found);
+    res.json(found);
+  });
+});
 
 app.post("/create", (req, res) => {
-    console.log("Create Route HIT", req.body)
-    ToDo.create(req.body)
-        .then(created => {
-            console.log("created", created)
-            res.json(created)
-        })
+  console.log("Create Route HIT", req.body);
+  ToDo.create(req.body).then((created) => {
+    console.log("created", created);
+    res.json(created);
+  });
+});
 
+app.delete("/delete/:id", (req, res) => {
+  console.log("Delete Route HIT", req.params.id);
+  ToDo.findByIdAndDelete(req.params.id)
+    .then((deleted) => {
+      console.log("deleted:", deleted);
+      res.json(deleted);
+    })
+    .catch((err) => {
+      console.log("Delete error:", err);
+      res.status(500).json(err);
+    });
+});
 
-
-})
+app.put("/update/:id", (req, res) => {
+  console.log("Update Route HIT", req.params.id, req.body);
+  ToDo.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updated) => {
+      console.log("updated:", updated);
+      res.json(updated);
+    })
+    .catch((err) => {
+      console.log("Update error:", err);
+      res.status(500).json(err);
+    });
+});
 
 app.listen(PORT, () => {
-
-    mongoose.connect(process.env.MONGO_URI)
-        .then(() => {
-            console.log("Connected to Database")
-        })
-    console.log(`Server is runnning on port ${PORT}`)
-})
+  mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connected to Database");
+  });
+  console.log(`Server is runnning on port ${PORT}`);
+});
