46ff3951dc5c5b08df375dcf73759a5d1e172eaa
Shawn Tapps <stapps.prsvr@gmail.com>
2025-01-23 18:00:35 -0600

    update1

--- lectures/Todo/client/Todo/src/App.jsx
+++ lectures/Todo/client/Todo/src/App.jsx
@@ -1,49 +1,41 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+
+import "./App.css";
+import axios from "axios";
 
 function App() {
   // const [count, setCount] = useState(0)
 
-  const [data, setData] = useState()
+  const [data, setData] = useState();
 
   useEffect(() => {
-
     axios({
       method: "get",
-      url: "http://localhost:3000/gettodos"
+      url: "http://localhost:3000/gettodos",
     })
-      .then(res => {
-        console.log("res", res)
-        setData(res.data)
+      .then((res) => {
+        console.log("res", res);
+        setData(res.data);
       })
-      .catch(err => console.log("err", err))
-
-
-  }, [])
-
+      .catch((err) => console.log("err", err));
+  }, []);
 
   return (
     <>
       {console.log("data", data)}
 
-   
-        {data && data.map((item) => {
+      {data &&
+        data.map((item) => {
           return (
-            <div style={{ border: '2px solid red' }}>
-
-            <p> {item.todo}</p>
-            <button>delete</button>
-            <button>edit</button>
-         
+            <div style={{ border: "2px solid red" }}>
+              <p> {item.todo}</p>
+              <button>delete</button>
+              <button>edit</button>
             </div>
-          )
+          );
         })}
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
