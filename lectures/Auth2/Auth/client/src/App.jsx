8b8372a14145ac70c7c369290e8f26a3ebcbf8d7
Shawn Tapps <stapps.prsvr@gmail.com>
2025-02-11 11:01:02 -0600

    test

--- lectures/Auth2/Auth/client/src/App.jsx
+++ lectures/Auth2/Auth/client/src/App.jsx
@@ -1,77 +1,103 @@
-import { useState } from 'react'
-import './App.css'
-import axios from 'axios'
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
   const [login, setLogin] = useState({
-    username: '',
-    password: ''
-  })
+    username: "",
+    password: "",
+  });
   const [register, setRegister] = useState({
-    username: '',
-    password: ''
-  })
+    username: "",
+    password: "",
+  });
   const handleLogin = (e) => {
-    console.log("login", e.target.value)
-    setLogin({ ...prev, [e.target.id]: e.target.value })
-  }
+    console.log("login", e.target.value);
+    setLogin((prev) => {
+      console.log("prev", prev);
+      return {
+        ...prev,
+        [e.target.id]: e.target.value,
+      };
+    });
+  };
   const handleLoginSubmit = () => {
-    console.log(login)
+    console.log(login);
     axios({
-      method: 'post',
-      url: 'http://localhost:3000/login',
-      data: login
+      method: "post",
+      url: "http://localhost:3000/login",
+      data: login,
     })
-      .then(res => console.log("res", res.data))
-      .catch(error => console.log(error))
-  }
+      .then((res) => console.log("res", res.data))
+      .catch((error) => console.log(error));
+  };
   const handleRegister = (e) => {
-    console.log("reg", register)
-    setRegister(prev => ({
+    console.log("reg", register);
+    setRegister((prev) => ({
       ...prev,
-      [e.target.id]: e.target.value
-    }))
-  }
+      [e.target.id]: e.target.value,
+    }));
+  };
   const handleRegisterSubmit = (e) => {
-    console.log("reg", register)
+    console.log("reg", register);
     axios({
-      method: 'post',
-      url: 'http://localhost:3000/register',
-      data: register
+      method: "post",
+      url: "http://localhost:3000/register",
+      data: register,
     })
-      .then(res => console.log("res", res.data))
-      .catch(error => console.log(error))
-  }
+      .then((res) => console.log("res", res.data))
+      .catch((error) => console.log(error));
+  };
   return (
     <>
-      <div id='login'>
+      <div id="login">
         {console.log("login", login)}
         {console.log("Reg", register)}
         <h1>Login</h1>
-        <input id='username' onChange={(e) => handleLogin(e)} type='text' placeholder='Username' />
+        <input
+          id="username"
+          onChange={(e) => handleLogin(e)}
+          type="text"
+          placeholder="Username"
+        />
         <br />
         <br />
-        <input id="password" onChange={(e) => handleLogin(e)} type='text' placeholder='Password' />
+        <input
+          id="password"
+          onChange={(e) => handleLogin(e)}
+          type="text"
+          placeholder="Password"
+        />
         <br />
         <br />
         <button onClick={() => handleLoginSubmit()}>Login</button>
       </div>
       <br />
       <br />
-<hr />
-<br />
-<br />
-      <div id='register'>
+      <hr />
+      <br />
+      <br />
+      <div id="register">
         <h1>Register</h1>
-        <input id='username' onChange={(e) => handleRegister(e)} type='text' placeholder='Username' />
+        <input
+          id="username"
+          onChange={(e) => handleRegister(e)}
+          type="text"
+          placeholder="Username"
+        />
         <br />
         <br />
-        <input id='password' onChange={(e) => handleRegister(e)} type='text' placeholder='Password' />
+        <input
+          id="password"
+          onChange={(e) => handleRegister(e)}
+          type="text"
+          placeholder="Password"
+        />
         <br />
         <br />
         <button onClick={() => handleRegisterSubmit()}>Register</button>
         <br />
       </div>
     </>
-  )
+  );
 }
-export default App
+export default App;
